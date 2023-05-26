1366fab2af387a8580df7b1c37f878144a624cbb
kou-oor <kuura4201@gmail.com>
2023-05-26 19:18:41 +0900

    all commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_17_001959) do
+ActiveRecord::Schema.define(version: 2023_05_24_082559) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
