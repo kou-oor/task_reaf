523dd64c6e99f44c09b3c866657f76e161c712ab
kou-oor <kuura4201@gmail.com>
2023-05-30 15:56:06 +0900

    1+Nのための関連付け

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,11 @@ class Task < ApplicationRecord
   # validates :name, length: {minimum:30}
   validate :validate_name_not_including_comma
 
+
+  # belongs_toはそのクラス(ここではTask)がある別のクラス(ここではUser)に従属しており、
+  # 従属先のクラスのidを外部キーとして抱えていることを表しています。いわば、親分を指定するような感じ
+  belongs_to :user
+
   private
   def validate_name_not_including_comma
     errors.add(:name, 'にカンマを含めることはできません') if name&.include?(",")
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,9 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
+
+  # has_manyはUserのidを外部キーとして抱えるような他のクラス(ここではTask)があり、
+  # そのレコードが複数登録可能であることを表している
+  has_many :tasks
 end
 
